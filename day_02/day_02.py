816c8c30158d04d5d9e15b38799437e0c7776ead
Elliot Yates <elliotyatesj@gmail.com>
2024-12-07 19:20:24 +0000

    Day 2 part a solution

---
+++ day_02/day_02.py
@@ -0,0 +1,22 @@
+number_safe_reports = 0
+
+with open("day_02_input.txt") as file:
+    for line in file:
+        report = [int(x) for x in line.strip().split()]
+        report_safe = True
+
+        increasing = all(report[i+1] - report[i] > 0 for i in range(len(report)-1))
+        decreasing = all(report[i+1] - report[i] < 0 for i in range(len(report)-1))
+
+        if not (increasing or decreasing):
+            report_safe = False
+
+        for i in range(len(report)-1):
+            diff = abs(report[i+1] - report[i])
+            if diff < 1 or diff > 3:
+                report_safe = False
+
+        if report_safe:
+            number_safe_reports += 1
+
+print(number_safe_reports)
